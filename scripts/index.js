903f880a912af4aabb0d74ca362ecab7c887e87d
harshxraj <harshraj0588@gmail.com>
2023-07-05 13:07:50 +0530

    landing page layout done

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 4,
+    },
+    700: {
+      slidesPerView: 3,
+    },
+    400: {
+      slidesPerView: 2,
+    },
+  },
+});
